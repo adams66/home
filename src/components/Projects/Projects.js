4ca2495dfa3f18c777e63d102787556b5e1d729d
adams66 <47400311+adams66@users.noreply.github.com>
2023-05-04 15:17:54 -0600

    adding card component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import * as helper from "../../helper/helper";
 import webSites from "../../key/projects.json";
+import Card from "./cards/Card";
 
 function Projects(props) {
     var websites = webSites[2].data;
@@ -20,34 +21,7 @@ function Projects(props) {
               {websites.map((w)=>{
                 return(
 
-<div style={{position: "relative"}} class="item padding-30 ">
-<a style={{ display: "block", position: "absolute", top: "0", right: "0", bottom: "0", width: "100%", zIndex:"2"}} href=""> </a>   
-   
-   <div class="card  flex flex-column flex-j-spacedBetween">
-       <div class="icon-container flex flex-j-spacedBetween margin-bottom-20">
-           <i  class="ri-folder-line icon-size color-text-Theme"></i>
-           <a class="text-decoration-none" href={w.link} target="_blank"> <i class="ri-external-link-line icon-color icon-size"></i></a>
-
-       </div>
-
-       <div class="card-heading margin-bottom-20 off-white">
-           <h3 class="sub-headings margin-bottom-10">{w.Name}</h3>
-           <p class="paragraphs">{w.About}</p>
-
-       </div>
-
-       <div class="tech">
-           <ul class="tech-list">
-
-
-           <li><p>{w.tech.tech1}</p></li>
-           <li><p>{w.tech.tech2}</p></li>
-           <li><p>{w.tech.tech3}</p></li>
-       
-           </ul>
-       </div>
-   </div>
-</div>
+                    <Card data={w} />
 
 
 
---
+++ src/components/Projects/cards/Card.js
@@ -0,0 +1,36 @@
+function Card(props) {
+    var data = props.data;
+
+
+
+    return ( <div style={{position: "relative"}} class="item padding-30 ">
+    <a style={{ display: "block", position: "absolute", top: "0", right: "0", bottom: "0", width: "100%", zIndex:"2"}} href=""> </a>   
+       
+       <div class="card  flex flex-column flex-j-spacedBetween">
+           <div class="icon-container flex flex-j-spacedBetween margin-bottom-20">
+               <i  class="ri-folder-line icon-size color-text-Theme"></i>
+               <a class="text-decoration-none" href={data.link} target="_blank"> <i class="ri-external-link-line icon-color icon-size"></i></a>
+    
+           </div>
+    
+           <div class="card-heading margin-bottom-20 off-white">
+               <h3 class="sub-headings margin-bottom-10">{data.Name}</h3>
+               <p class="paragraphs">{data.About}</p>
+    
+           </div>
+    
+           <div class="tech">
+               <ul class="tech-list">
+    
+    
+               <li><p>{data.tech.tech1}</p></li>
+               <li><p>{data.tech.tech2}</p></li>
+               <li><p>{data.tech.tech3}</p></li>
+           
+               </ul>
+           </div>
+       </div>
+    </div> );
+}
+
+export default Card;
